5402e6179119686e416c39d734f12dcb49a49e9e
Ivan Bondarenko <bondarenko.ivan.v@gmail.com>
2016-02-28 23:05:01 +0100

    Update TestModel.py

--- TestModel.py
+++ TestModel.py
@@ -1,4 +1,4 @@
-# Databricks notebook source exported at Sun, 28 Feb 2016 22:04:32 UTC
+# Databricks notebook source exported at Sun, 28 Feb 2016 22:05:02 UTC
 # MAGIC %sql select * from test_redshift
 
 # COMMAND ----------
